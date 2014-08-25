0ac592ca641adfb1e8b805da97b3d1b9b41c4ae7
Napon Taratan <napon@napontaratan.com>
2014-08-25 11:56:30 -0700

    added video hover transition, moved code from nav_script to main.js

--- js/main.js
+++ js/main.js
@@ -6,6 +6,47 @@ $( document ).ready(function() {
     $("body").animate({scrollTop: $("#who-bruce").offset().top+75}, 'slow');
   });
 
+    // product hover
+    $('.product-item').hover(function() {
+      $(this).find('.product-name').fadeIn('fast');
+    }, function() {
+      $(this).find('.product-name').fadeOut('fast');
+    });
+
+    // menu hover
+    var video1 = document.getElementById('bg-video');
+    var video2 = document.getElementById('amps-video');
+    $('#local_band').hover(function() {
+      video1.classList.add('hide');
+      video2.classList.remove('hide');
+      video2.play();
+    }, function() {
+      video2.classList.add('hide');
+      video1.classList.remove('hide');
+      video1.play();
+    });
+     video1 = document.getElementById('bg-video');
+    var video2 = document.getElementById('amps-video');
+    $('#local_guitarist').hover(function() {
+      video1.classList.add('hide');
+      video2.classList.remove('hide');
+      video2.play();
+    }, function() {
+      video2.classList.add('hide');
+      video1.classList.remove('hide');
+      video1.play();
+    });
+     video1 = document.getElementById('bg-video');
+    var video2 = document.getElementById('amps-video');
+    $('#local_artist').hover(function() {
+      video1.classList.add('hide');
+      video2.classList.remove('hide');
+      video2.play();
+    }, function() {
+      video2.classList.add('hide');
+      video1.classList.remove('hide');
+      video1.play();
+    });
 
     // Back to Resources button
     // $(document).ready(function() {
